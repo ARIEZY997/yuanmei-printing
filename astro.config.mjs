8ec617f14209c66fffdf25c3ad40da7ce0a1282d
unknown <769727279@qq.com>
2026-08-06 17:24:57 +0800

    Fix sitemap domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://yuanmeiprinting.com",
+  site: "https://ymboxs.com",
   output: "static",
   trailingSlash: "always",
   integrations: [sitemap()],
